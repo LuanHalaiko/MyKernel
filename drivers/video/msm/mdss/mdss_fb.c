5e969e93b977f32efc66dfe86401941a9394064d
flar2 <asegaert@gmail.com>
2016-06-26 23:06:11 -0400

    mdss_fb: add backlight dimmer option
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>
    Signed-off-by: Haseo97 <jawgattus@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -91,6 +91,12 @@ extern int LCM_effect[3];
  */
 #define MDP_TIME_PERIOD_CALC_FPS_US	1000000
 
+#define MDSS_BRIGHT_TO_BL_DIM(out, v) do {\
+			out = (12*v*v+1393*v+3060)/4465;\
+			} while (0)
+bool backlight_dimmer = false;
+module_param(backlight_dimmer, bool, 0755);
+
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
 
